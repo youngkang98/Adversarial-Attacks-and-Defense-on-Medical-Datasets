6953726dce2e151fcd1bc0fcaa9c0f52e400cd70
lkangSwinburne <lkang@swinburne.edu.my>
2024-07-03 13:44:34 +0800

    Add in CIFAR 10 adv train method

--- SeparateImageToFolderWithCSV.py
+++ SeparateImageToFolderWithCSV.py
@@ -9,7 +9,7 @@ import pandas as pd
 import shutil
 import os
 
-def organize_images_by_label(csv_path, image_directory, output_directory):
+def organize_images_by_label_column(csv_path, image_directory, output_directory):
     """
     Organize images into folders based on their labels.
 
@@ -24,7 +24,7 @@ def organize_images_by_label(csv_path, image_directory, output_directory):
     # Iterate over the DataFrame
     for index, row in df.iterrows():
         # Get the image file name and label
-        image_file = f"{row['image']}.jpg"
+        image_file = f"{row['image_id']}"
         label = row['label']
         
         # Create a directory for the label if it doesn't exist
@@ -42,10 +42,57 @@ def organize_images_by_label(csv_path, image_directory, output_directory):
             print(f"Copied {src_path} to {dest_path}")
         else:
             print(f"Warning: {src_path} does not exist.")
+            
+def organize_images_by_class(csv_path, image_directory, output_directory):
+    """
+    Organize images into folders based on their labels.
+
+    Parameters:
+    - csv_path: Path to the CSV file containing 'image' and label columns.
+    - image_directory: Directory where the images currently reside.
+    - output_directory: Directory where the labeled folders will be created.
+    """
+    # Read the CSV file
+    df = pd.read_csv(csv_path)
+    
+    # Get the label columns
+    label_columns = df.columns[1:]  # Assuming the first column is 'image'
+
+    # Iterate over the DataFrame
+    for index, row in df.iterrows():
+        # Get the image file name
+        image_file = f"{row['image']}.jpg"
+        
+        # Construct the source path
+        src_path = os.path.join(image_directory, image_file)
+        
+        # Check each label column
+        for label in label_columns:
+            if row[label] == 1:
+                # Create a directory for the label if it doesn't exist
+                label_dir = os.path.join(output_directory, label)
+                if not os.path.exists(label_dir):
+                    os.makedirs(label_dir)
+
+                # Construct the destination path
+                dest_path = os.path.join(label_dir, image_file)
+
+                # Check if the source file exists before copying
+                if os.path.exists(src_path):
+                    shutil.copy(src_path, dest_path)
+                    print(f"Copied {src_path} to {dest_path}")
+                else:
+                    print(f"Warning: {src_path} does not exist.")
 
 if __name__ == "__main__":
-    csv_path = 'C:/Users/lkang/Documents/Adversarial-Attack-and-Defense/ISIC2019_test.csv'  # Path to the CSV file
-    image_directory = 'C:/Users/lkang/Documents/ISIC_2019_Training_Input/'  # Directory containing the images
-    output_directory = 'C:/Users/lkang/Documents/ISIC_2019_test/'  # Directory where folders will be created
+    ###### ISIC 2019 ################
+    # csv_path = 'C:/Users/lkang/Documents/Adversarial-Attack-and-Defense/ISIC2019_test.csv'  # Path to the CSV file
+    # image_directory = 'C:/Users/lkang/Documents/ISIC_2019_Training_Input/'  # Directory containing the images
+    # output_directory = 'C:/Users/lkang/Documents/ISIC_2019_test/'  # Directory where folders will be created
+    ###### ISIC 2018 ##########
+    # csv_path = 'C:/Users/lkang/Documents/isic2018/ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth.csv'  # Path to the CSV file
+    csv_path = 'C:/Users/lkang/Documents/df_val.csv'  # Path to the CSV file
+    image_directory = 'C:\\Users\\lkang\\Documents\\isic2018\\ISIC2018_Task3_Training_Input\\'  # Directory containing the images
+    output_directory = 'C:\\Users\\lkang\\Documents\\isic2018\\dataset'  # Directory where folders will be created
 
-    organize_images_by_label(csv_path, image_directory, output_directory)
+    organize_images_by_label_column(csv_path, image_directory, output_directory)
--- utils/test.py
+++ utils/test.py
@@ -7,10 +7,9 @@ Created on Sat Apr 20 19:10:13 2024
 import torch
 import torch.nn.utils.prune as prune
 from torch.autograd import Variable
-from cbam.imagenet import create_resnet
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import os
+from sklearn.metrics import balanced_accuracy_score, roc_auc_score
+import torch.nn.functional as F
+import numpy as np
 
 def test(model, loader, name):
     model.cuda()
@@ -27,5 +26,48 @@ def test(model, loader, name):
             1, keepdim=True)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
+    print('\n{}: Accuracy: {}/{} ({:.0f}%)\n'.format(name, correct,
+          len(loader.dataset), 100. * correct / len(loader.dataset)))
+    
+def test2(model, loader, name):
+    model.cuda()
+    model.eval()
+    device = 'cuda'
+    y_true = []
+    y_pred = []
+    y_score=[]
+    correct = 0
+    val_acc = 0
+    with torch.no_grad():
+        for _, data in enumerate(loader):
+            images, labels = data
+            images = Variable(images).to(device)
+            labels = Variable(labels).to(device)
+
+            outputs = model(images)
+            # pred = outputs.data.max(
+            #     1, keepdim=True)[1]  # get the index of the max log-probability
+            # correct += pred.eq(labels.data.view_as(pred)).cpu().sum()
+            probs = torch.softmax(outputs,dim=1)
+            prediction = outputs.max(1, keepdim=True)[1]
+            y_true.append(labels.cpu().numpy())
+            y_pred.append(prediction.detach().cpu().numpy())
+            y_score.append(probs.detach().cpu().numpy())
+
+        y_true = np.concatenate(y_true)
+        y_true_2D= F.one_hot(torch.from_numpy(y_true), num_classes=7).cpu().numpy()
+        y_pred = np.concatenate(y_pred)
+        y_score = np.concatenate(y_score)
+        test_bacc = balanced_accuracy_score(y_true, y_pred)
+        test_auc=roc_auc_score(y_true_2D, y_score)
+            # probs = torch.softmax(outputs,dim=1)
+            # prediction = outputs.max(1, keepdim=True)[1]
+            # y_true.append(labels.cpu().numpy())
+            # y_pred.append(prediction.detach().cpu().numpy())
+            # y_score.append(probs.detach().cpu().numpy())
+            # val_acc += torch.sum(y_pred == y_true)
+    
+    print(f'Balanced Accuracy:{test_bacc}')
+    print(f'ROC AUC Accuracy:{test_bacc}')
     print('\n{}: Accuracy: {}/{} ({:.0f}%)\n'.format(name, correct,
           len(loader.dataset), 100. * correct / len(loader.dataset)))
